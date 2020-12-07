43df35d8ac5432fde53a02a936617ab88eae8a71
Lucha3sr <2278@holbertonschool.com>
2020-12-07 21:26:15 +0000

    Python

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - frees a listint_t list
+ * @list: pointer to list
+ * Return: void
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *p1 = list;
+	listint_t *p2 = list;
+
+	while (p1->next != NULL && p2->next != NULL)
+	{
+		p1 = p1->next;
+		p2 = p2->next;
+		if (p2->next == NULL)
+		{
+			return (0);
+		}
+		p2 = p2->next;
+		if (p1 == p2)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
