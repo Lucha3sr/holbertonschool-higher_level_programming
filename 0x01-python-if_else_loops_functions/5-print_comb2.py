3ea0ce3bb6b7b1b5ee661dc5b5f99469f98ce76b
Lucha3sr <2278@holbertonschool.com>
2020-12-08 19:12:02 +0000

    Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for nbr in range(100):
+    if nbr < 99:
+        print("{:02d}, " .format(nbr), end="")
+    else:
+        print(nbr)
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def uppercase(str):
+for i in str:
+
+        return True
+    else:
+        return False
