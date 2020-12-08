824a2b0023e1f35eccac443cc4c80e770ee364fd
Lucha3sr <2278@holbertonschool.com>
2020-12-08 21:30:40 +0000

    Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+def fizzbuzz():
+    for nbr in range(1, 101):
+        if nbr % 3 == 0 and nbr % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif nbr % 5 == 0:
+            if nbr == 100:
+                print("Buzz", end=" ")
+            else:
+                print("Buzz", end=" ")
+        elif nbr % 3 == 0:
+            print("Fizz", end=" ")
+        else:
+            print(nbr, end=" ")
