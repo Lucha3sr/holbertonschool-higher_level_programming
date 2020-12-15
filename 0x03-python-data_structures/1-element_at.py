703e4ad7d770d997b3ad6b0faa45874eec60578e
Lucha3sr <2278@holbertonschool.com>
2020-12-15 15:17:39 +0000

    Data Structures: Lists, Tuples

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,8 @@
 def element_at(my_list, idx):
     total_elements = len(my_list)
     if idx < 0:
-        return none
+        return None
     elif idx > total_elements - 1:
-        return none
+        return None
     else:
         return (my_list[idx])
