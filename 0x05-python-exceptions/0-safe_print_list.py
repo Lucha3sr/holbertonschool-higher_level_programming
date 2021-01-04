a36779cceaca2fdfad90fc28227663155d1cde83
Lucha3sr <2278@holbertonschool.com>
2021-01-04 16:00:43 +0000

    Python - Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,10 @@ def safe_print_list(my_list=[], x=0):
 
     realnbr = 0
     try:
-        for i in range x:
-            print("{}" .format(my_list=[i]), end="")
+        for i in range (x):
+            print("{}" .format(my_list[i]), end="")
             realnbr += 1
-    except IndexError:
-        print("index error")
-        return (realnbr)
+    except:
+        pass
+    print()
+    return realnbr
