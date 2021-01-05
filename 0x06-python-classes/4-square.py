5c0351fcd17aec45809d96a4595697f0615c7abc
Lucha3sr <2278@holbertonschool.com>
2021-01-05 21:07:37 +0000

    Python - Classes and Objects

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -24,14 +24,14 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, size):
+    def size(self, value):
 
         """setting the value"""
-        if type(size) is not int:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """returns the current area"""
