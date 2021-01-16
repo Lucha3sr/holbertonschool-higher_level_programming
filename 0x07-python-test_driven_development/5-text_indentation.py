4950832fad3e8aaa7d1221a9fb05f80c39379f05
Lucha3sr <2278@holbertonschool.com>
2021-01-16 04:19:53 +0000

    Python - Test-driven development

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -12,20 +12,17 @@ def text_indentation(text):
     """
     Prints a text with 2 new lines after each of these characters: ., ? and :
     """
-    if type(text) is not str:
-        raise TypeError('text must be a string')
     blank_space = 0
-    for char in text:
-        if blank_space == 0:
-            if char == " ":
-                continue
-            else:
-                print(char, end="")
-                blank_space = 1
-        else:
-            if char == "?" or char == "." or char == ":":
-                print(char, end="")
-                print("\n")
+    if type(text) == str:
+        for letter in text:
+            if blank_space == 1:
                 blank_space = 0
+                continue
+            if letter == "." or letter == "?" or letter == ":":
+                print(letter, end="")
+                print("\n")
+                blank_space = 1
             else:
-                print(char, end="")
+                print(letter, end="")
+    else:
+        raise TypeError("text must be a string")
