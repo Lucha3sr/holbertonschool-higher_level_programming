2e417dbfef7a7c9db7d9f718559eb3553b303282
Lucha3sr <2278@holbertonschool.com>
2021-01-20 19:52:30 +0000

    Python - Input/Output

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""class Student that defines a student"""
+
+class Student:
+
+"""Public instance attributes: first_name, last_name, age"""
+
+
+def __init__(self, first_name, last_name, age):
+
+    """Instantiation with first_name, last_name and age"""
+
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
+
+    def to_json(self):
+
+        """retrieves a dictionary representation of a Student instance"""
+        self.__dict__
+
+    def to_json(self, attrs=None):
+
+        """retrieves a dictionary representation of a Student instance"""
+
+        if type(attrs) is list:
+            
