7f06502f5a9567bf7d1b073c8c8fc6e5ef483c3c
Lucha3sr <2278@holbertonschool.com>
2021-01-20 02:51:19 +0000

    Python - Inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+""" class Square that inherits from Rectangle (9-rectangle.py)"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+
+    """Instantiation with size: def __init__(self, size)"""
+
+    def __init__(self, size):
+
+        """ size must be private. No getter or setter"""
+        """size must be a positive integer, validated by integer_validator"""
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+        def area(self):
+            """ the area() method implemented"""
+
+            return self.__size * self.__size
