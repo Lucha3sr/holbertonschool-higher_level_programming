deeb8df9c4d28795912087a7461bf6fa160dc36d
Lucha3sr <2278@holbertonschool.com>
2021-01-21 02:03:05 +0000

    Python - Input/Output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -2,27 +2,38 @@
 
 """class Student that defines a student"""
 
+
 class Student:
 
-"""Public instance attributes: first_name, last_name, age"""
+    """Public instance attributes: first_name, last_name, age"""
 
+    def __init__(self, first_name, last_name, age):
 
-def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
 
-    """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
-
-    def to_json(self):
-
-        """retrieves a dictionary representation of a Student instance"""
-        self.__dict__
 
     def to_json(self, attrs=None):
 
         """retrieves a dictionary representation of a Student instance"""
 
-        if type(attrs) is list:
-            
+        if attrs is None:
+
+            return self.__dict__
+
+        new_dict = {}
+
+        for attr in attrs:
+
+            if type(attr) is not str:
+
+                return self.__dict__
+
+            if attr in self.__dict__:
+
+                new_dict[attr] = self.__dict__[attr]
+
+        return new_dict
