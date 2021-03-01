48b3f3f87f1700988ea5f8ccc3ba9606bd979b3d
Lucha3sr <2278@holbertonschool.com>
2021-03-01 20:08:50 +0000

    SQL - Introduction

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- inserts a new row in the table first_table
-INSERT INTO table(id = 89, name = Holberton School);
+INSERT INTO first_table VALUES (89, 'Holberton School');
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- script that displays the number of records with id = 89 in first table
+SELECT COUNT(id = 89) FROM first_table
