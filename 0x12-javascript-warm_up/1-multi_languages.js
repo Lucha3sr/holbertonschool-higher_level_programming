0983198bf71020e5738b1856f03339b6765fe3c2
Lucha3sr <2278@holbertonschool.com>
2021-03-22 17:05:00 +0000

    JavaScript - Warm up

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVarC = 'C is fun';
+const myVarPy = 'Python is cool';
+const myVarJs = 'JavaScript is amazing';
+console.log(myVarC);
+console.log(myVarPy);
+console.log(myVarJs);
