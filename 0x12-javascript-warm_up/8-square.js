b7d55010be19f46cca5847df6fc3d013edf50a49
Lucha3sr <2278@holbertonschool.com>
2021-03-22 18:54:54 +0000

    JavaScript - Warm up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const firstArg = parseInt(process.argv[2]);
-if ( isNaN(firstArg) === false) {
+if (isNaN(firstArg) === false) {
   let i;
   let ii;
   for (i = 0; i < firstArg; i++) {
     for (ii = 0; ii < firstArg; ii++) {
       process.stdout.write('X');
     }
-    console.log(' ');
+    console.log();
   }
 } else {
   console.log('Missing size');
