44e17a8eea8418caa1b39cd93eb822be84b49d68
Lucha3sr <2278@holbertonschool.com>
2021-03-27 04:36:48 +0000

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,10 +3,9 @@
 """script that takes in an argument and displays all values in the states
 table of hbtn_0e_0_usa where name matches the argument"""
 
+from sys import argv
+import MySQLdb
 if __name__ == '__main__':
-
-    from sys import argv
-    import MySQLdb
 
     MY_USER = argv[1]
     MY_PASS = argv[2]
