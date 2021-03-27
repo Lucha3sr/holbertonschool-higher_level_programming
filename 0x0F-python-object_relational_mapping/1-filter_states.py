2e2befce6949b38b7bc0c95e4d4f0cb52b0a95d2
Lucha3sr <2278@holbertonschool.com>
2021-03-27 04:04:03 +0000

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,7 +2,7 @@
 
 """Script that lists all states from the database hbtn_0e_0_usa"""
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     from sys import argv
     import MySQLdb
@@ -14,7 +14,9 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=MY_USER,
                          passwd=MY_PASS, db=MY_DB)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'
+                ORDER BY states.id ASC")
+
     rows = cur.fetchall()
 
     for r in rows:
