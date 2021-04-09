da7d9d95e76fb0de78d47175f4082492208d0823
Lucha3sr <2278@holbertonschool.com>
2021-04-09 19:59:59 +0000

    Python - Network #1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+""" sends a request to the URL and displays the value of the X-Request-Id"""
+
 import urllib.request
 from sys import argv
-
-"""Send a request to the URL and displays the value of the X-Request-Id"""
 
 if __name__ == "__main__":
     req = urllib.request.Request(argv[1])
