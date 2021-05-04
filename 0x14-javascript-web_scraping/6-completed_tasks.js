5b67d7a453ff6b1c0e413449b3e6a986cfa881ec
Lucha3sr <2278@holbertonschool.com>
2021-05-04 20:06:59 +0000

    JavaScript - Web scraping

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (response, body) {
+  const tasks = (JSON.parse(body.body));
+  const tasksDone = {};
+  let count = 0;
+  for (const task in tasks) {
+    const userID = tasks[task].userId;
+    if (!(userID in tasksDone)) {
+      count = 0;
+    }
+    if (tasks[task].completed) {
+      count += 1;
+    }
+    tasksDone[userID] = count;
+  }
+  console.log(tasksDone);
+});
