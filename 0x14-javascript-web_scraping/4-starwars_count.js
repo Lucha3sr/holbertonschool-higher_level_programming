a462a994b20be0c2521babb0cf5093a97664c1e6
Lucha3sr <2278@holbertonschool.com>
2021-05-04 18:10:02 +0000

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
 const urlId = process.argv[2] + '/?format=json';
-const peopleNbr = 'https://swapi-api.hbtn.io/api/people/18/';
 request.get(urlId, function (response, body) {
   const filmList = (JSON.parse(body.body).results);
   let count = 0;
   for (const film in filmList) {
     for (const chars in filmList[film].characters) {
-      if (filmList[film].characters[chars] === peopleNbr) {
+      if (filmList[film].characters[chars].includes('18')) {
         count++;
       }
     }
