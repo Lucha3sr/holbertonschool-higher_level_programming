b069355e69a2cf5516616d4157d379b627f7e94e
Lucha3sr <2278@holbertonschool.com>
2021-05-04 16:57:34 +0000

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,5 +2,5 @@
 const request = require('request');
 const urlId = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/?format=json';
 request.get(urlId, function (response, body) {
-    console.log(JSON.parse(body.body).title);
+  console.log(JSON.parse(body.body).title);
 });
